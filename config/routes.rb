77157208e9b1795764fddfa5729346d2b13f134a
okothkongo1 <okothkongo@gmail.com>
2019-04-24 18:50:14 +0300

    user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v0 do
+      namespace :User do
+       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+      end
+    end
+  end
 end
