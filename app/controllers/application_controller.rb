34aefcc3da7e7dad93dacdf38805b1ee01813826
okothkongo1 <okothkongo@gmail.com>
2019-04-25 12:38:03 +0300

    allow  unpermitted user params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number])
+  end
 end
